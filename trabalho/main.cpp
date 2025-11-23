a3773b1ca0203602e217c62eb009a30b20d4db0d
Thiago Martins <108444333+ThiagoMartins04@users.noreply.github.com>
2025-11-23 19:33:48 -0300

    Aula 17-11
    Commit das alterações realizadas em aula no dia 17/11

--- trabalho/main.cpp
+++ trabalho/main.cpp
@@ -1,8 +1,13 @@
 #include "raylib.h"
+#include <iostream>
+
+using namespace std;
 
 // Plataforma (player)
 Vector2 plataformaPosicao = {350.0f, 550.0f};
+Vector2 plataformaPosicaoFim = {450.0f, 550.0f};
 Vector2 plataformaTamanho = {100.0f, 20.0f};
+Color cor = MAROON;
 
 // Pelota
 Vector2 bolaPosicao = {400.0f, 539.0f};
@@ -15,15 +20,41 @@ float bolaVelocidadeY = 10.0f;
 float bolaVelocidadeX = 10.0f;
 bool moveDiagonal = false;
 
+struct Blocos {
+  Rectangle bloco;
+  Color cor;
+  bool ativo = true;
+};
+
 int main() {
   const int screenWidth = 800;
   const int screenHeight = 600;
   InitWindow(screenWidth, screenHeight, "raylib basic window");
   SetTargetFPS(60);
+
+  //Blocos do ARKANOID
+  Blocos blocos[6];
+  Color coresBloco = GREEN;
+  float posBlocosX = -120.0f;
+  for (int i = 0; i < 6; i++) {
+    posBlocosX += 130.0f;
+    blocos[i].bloco.x = posBlocosX;
+    blocos[i].bloco.y = 200.0f;
+    blocos[i].bloco.height = 50.0f;
+    blocos[i].bloco.width = 130.0f;
+    blocos[i].cor = coresBloco;
+  }
+
   while (!WindowShouldClose()) {
     // Movimentação do player
-    if (IsKeyDown(KEY_RIGHT) && plataformaPosicao.x < screenWidth - 110) plataformaPosicao.x += 10.0f;
-    if (IsKeyDown(KEY_LEFT)  && plataformaPosicao.x > 10)               plataformaPosicao.x -= 10.0f;
+    if (IsKeyDown(KEY_RIGHT) && plataformaPosicao.x < screenWidth - 110) {
+      plataformaPosicao.x += 10.0f;
+      plataformaPosicaoFim.x += 10.0f;
+    }
+    if (IsKeyDown(KEY_LEFT)  && plataformaPosicao.x > 10){
+      plataformaPosicao.x -= 10.0f;
+      plataformaPosicaoFim.x -= 10.0f;
+    }
 
     // Movimentação da bola
     if (bolaPosicao.y <= bolaTamanho) direcao = direcao * -1;
@@ -32,13 +63,15 @@ int main() {
       // Vai verificar se a posição que a bola bateu na plataforma está mais a esqueda ou direita
       if (bolaPosicao.x - plataformaPosicao.x < 50) {
         //Mais a esquerda
-        bolaVelocidadeX = 10.0f * (((bolaPosicao.x - plataformaPosicao.x) / 50);
+        bolaVelocidadeX = 10.0f * ((50 - (bolaPosicao.x - plataformaPosicao.x)) * 0.02f);
+        cout << bolaPosicao.x << " - " << plataformaPosicao.x << " = " << bolaPosicao.x - plataformaPosicao.x << endl;
         moveDiagonal = true;
         diagonal = -1;
       }
       if (bolaPosicao.x - plataformaPosicao.x > 50) {
         //Mais a direita
         bolaVelocidadeX = 10.0f * ((bolaPosicao.x - plataformaPosicao.x - 50) / 50);
+        
         moveDiagonal = true;
         diagonal = 1;
       }
@@ -57,13 +90,28 @@ int main() {
     }
     bolaVelocidadeY = 10.0f; // Vai voltar a velocidade padrão (para caso tenha mudado na funcao)
     
+    // Verifica colisão
+    for (int i = 0; i < 6; i++) {
+      if(CheckCollisionCircleRec(bolaPosicao, 10.0f, blocos[i].bloco)) {
+        blocos[i].cor = RED;
+        direcao *= -1;
+      }
+    }
+    
+    if (CheckCollisionCircleLine(bolaPosicao, 10.0f, plataformaPosicao, plataformaPosicaoFim)) {
+      cor = LIME;
+    }
+
 
     // Começa a pintar
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawText(TextFormat("%f", plataformaPosicao.x), 20, 20, 20, BLACK);
     DrawText(TextFormat("%f", bolaPosicao.y), 20, 50, 20, BLACK);
-    DrawRectangleV(plataformaPosicao, plataformaTamanho, MAROON);
+    for (int i = 0; i < 6; i++) {
+      DrawRectangle(blocos[i].bloco.x, blocos[i].bloco.y, blocos[i].bloco.width, blocos[i].bloco.height, blocos[i].cor);
+    }
+    DrawRectangleV(plataformaPosicao, plataformaTamanho, cor);
     DrawCircleV(bolaPosicao, 10, GREEN);
     
     EndDrawing();
