37880dfb3fe008d732aba99baee013a7b14d0ae7
Thiago Martins <108444333+ThiagoMartins04@users.noreply.github.com>
2025-11-23 23:50:01 -0300

    23/11
    - Alterado lógica para colisão da bola com os cantos e plataforma.
    - Adicionado mais blocos no formato 3x12
    - Começado lógica para colisão da bola com os blocos, falta verificar uma forma para quando bater na quina dos blocos
    - Alterado alguns valores que se repetiam para serem variáveis constantes

--- trabalho/main.cpp
+++ trabalho/main.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+// CONSTANTES - BLOCOS
+#define QUANTIDADEBLOCOS 12
+#define TAMANHOBLOCO 65.0f
+#define ALTURABLOCO 50.0f
+#define QUANTIDADELINHASBLOCOS 3
+// CONSTANTES - BOLA
+#define SPEEDBOLA 3.0f
+#define TAMANHOBOLA 10.0f
+
 // Plataforma (player)
 Vector2 plataformaPosicao = {350.0f, 550.0f};
 Vector2 plataformaPosicaoFim = {450.0f, 550.0f};
@@ -11,13 +20,14 @@ Color cor = MAROON;
 
 // Pelota
 Vector2 bolaPosicao = {400.0f, 539.0f};
+Vector2 bolaPosicaoFutura;
 const int bolaTamanho = 10;
 
 // Movimentação da bola
 int direcao = -1;
 int diagonal = 1; // 1 - Direita | -1 - Esquerda
-float bolaVelocidadeY = 10.0f;
-float bolaVelocidadeX = 10.0f;
+float bolaVelocidadeY = SPEEDBOLA;
+float bolaVelocidadeX = SPEEDBOLA;
 bool moveDiagonal = false;
 
 struct Blocos {
@@ -33,74 +43,159 @@ int main() {
   SetTargetFPS(60);
 
   //Blocos do ARKANOID
-  Blocos blocos[6];
+  Blocos blocos[3][12];
   Color coresBloco = GREEN;
-  float posBlocosX = -120.0f;
-  for (int i = 0; i < 6; i++) {
-    posBlocosX += 130.0f;
-    blocos[i].bloco.x = posBlocosX;
-    blocos[i].bloco.y = 200.0f;
-    blocos[i].bloco.height = 50.0f;
-    blocos[i].bloco.width = 130.0f;
-    blocos[i].cor = coresBloco;
+  float posBlocosX = (screenWidth - (QUANTIDADEBLOCOS * TAMANHOBLOCO)) / 2;
+  float posBlocosY = 10.0f;
+  int cont = 1;
+  for (int i = 0; i < QUANTIDADELINHASBLOCOS; i++) {
+    posBlocosX = 10.0f;
+    for (int j = 0; j < QUANTIDADEBLOCOS; j++) {
+      blocos[i][j].bloco.x = posBlocosX;
+      blocos[i][j].bloco.y = posBlocosY;
+      blocos[i][j].bloco.height = 50.0f;
+      blocos[i][j].bloco.width = TAMANHOBLOCO;
+      posBlocosX += TAMANHOBLOCO;
+      if (cont % 2 == 0) {
+        blocos[i][j].cor = BLUE;
+      } else {
+        blocos[i][j].cor = RED;
+      }
+      cont++;
+    }
+    cont++;
+    posBlocosY += 50.0f;
   }
 
   while (!WindowShouldClose()) {
     // Movimentação do player
-    if (IsKeyDown(KEY_RIGHT) && plataformaPosicao.x < screenWidth - 110) {
+    if ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) && plataformaPosicao.x < screenWidth - 110) {
       plataformaPosicao.x += 10.0f;
       plataformaPosicaoFim.x += 10.0f;
     }
-    if (IsKeyDown(KEY_LEFT)  && plataformaPosicao.x > 10){
+    if ((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))  && plataformaPosicao.x > 10){
       plataformaPosicao.x -= 10.0f;
       plataformaPosicaoFim.x -= 10.0f;
     }
 
     // Movimentação da bola
-    if (bolaPosicao.y <= bolaTamanho) direcao = direcao * -1;
-    if (bolaPosicao.y >= plataformaPosicao.y - bolaTamanho && bolaPosicao.x >= plataformaPosicao.x && bolaPosicao.x <= plataformaPosicao.x + plataformaTamanho.x) {
+
+    //Se bater na plataforma:
+    if (CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, plataformaPosicao, plataformaPosicaoFim)) {
       direcao *= -1;
       // Vai verificar se a posição que a bola bateu na plataforma está mais a esqueda ou direita
       if (bolaPosicao.x - plataformaPosicao.x < 50) {
         //Mais a esquerda
-        bolaVelocidadeX = 10.0f * ((50 - (bolaPosicao.x - plataformaPosicao.x)) * 0.02f);
-        cout << bolaPosicao.x << " - " << plataformaPosicao.x << " = " << bolaPosicao.x - plataformaPosicao.x << endl;
+        bolaVelocidadeX = SPEEDBOLA * ((50 - (bolaPosicao.x - plataformaPosicao.x)) * 0.02f);
         moveDiagonal = true;
         diagonal = -1;
       }
       if (bolaPosicao.x - plataformaPosicao.x > 50) {
         //Mais a direita
-        bolaVelocidadeX = 10.0f * ((bolaPosicao.x - plataformaPosicao.x - 50) / 50);
-        
+        bolaVelocidadeX = SPEEDBOLA * ((bolaPosicao.x - plataformaPosicao.x - 50) / 50);
         moveDiagonal = true;
         diagonal = 1;
       }
     }
-    if (bolaPosicao.x >= 790) diagonal *= -1;
-    if (bolaPosicao.x <= 10) diagonal *= -1;
-    
-    if (bolaPosicao.y < 20 && bolaPosicao.y > 10) {
-      // Vai calcular a distancia que falta para a plataforma e mudar a velocidade para que a bola não ultrapasse a plataforma
-      bolaVelocidadeY = bolaPosicao.y - 10;
+
+    if(CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, {0.0f, 10.0f}, {800.0f, 10.0f})) {
+      direcao *= -1; //Se bater no canto superior vai pra baixo
+    }
+    if(CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, {10.0f, 0.0f}, {10.0f, 600.0f})) {
+      diagonal = 1; // Se bater no canto esquerdo vai para a direita
+    }
+    if(CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, {790.0f, 0.0f}, {790.0f, 600.0f})) {
+      diagonal = -1; // Se bater no cando direito vai para a esquerda
     }
 
     bolaPosicao.y = bolaPosicao.y + (bolaVelocidadeY * direcao);
     if(moveDiagonal) {
       bolaPosicao.x = bolaPosicao.x + (bolaVelocidadeX * diagonal);
     }
-    bolaVelocidadeY = 10.0f; // Vai voltar a velocidade padrão (para caso tenha mudado na funcao)
-    
+
+    // Pega a posição futura para evitar que a bola ultrapasse o bloco
+    if (moveDiagonal) {
+      bolaPosicaoFutura = {bolaPosicao.x + bolaVelocidadeX, bolaPosicao.y + SPEEDBOLA};
+    } else {
+      bolaPosicaoFutura = {bolaPosicao.x, bolaPosicao.y + SPEEDBOLA};
+    }
+
     // Verifica colisão
-    for (int i = 0; i < 6; i++) {
-      if(CheckCollisionCircleRec(bolaPosicao, 10.0f, blocos[i].bloco)) {
-        blocos[i].cor = RED;
-        direcao *= -1;
-      }
+    for (int i = 0; i < QUANTIDADELINHASBLOCOS; i++) {
+        for (int j = 0; j < QUANTIDADEBLOCOS; j++) {
+            // Pontos laterais
+            Vector2 esq1 = {blocos[i][j].bloco.x, blocos[i][j].bloco.y};
+            Vector2 esq2 = {blocos[i][j].bloco.x, blocos[i][j].bloco.y + ALTURABLOCO};
+
+            Vector2 dir1 = {blocos[i][j].bloco.x + TAMANHOBLOCO, blocos[i][j].bloco.y};
+            Vector2 dir2 = {blocos[i][j].bloco.x + TAMANHOBLOCO, blocos[i][j].bloco.y + ALTURABLOCO};
+
+            // Pontos superior e inferior
+            Vector2 cima1 = {blocos[i][j].bloco.x, blocos[i][j].bloco.y + ALTURABLOCO};
+            Vector2 cima2 = {blocos[i][j].bloco.x + TAMANHOBLOCO, blocos[i][j].bloco.y + ALTURABLOCO};
+            
+            Vector2 baixo1 = {blocos[i][j].bloco.x, blocos[i][j].bloco.y};
+            Vector2 baixo2 = {blocos[i][j].bloco.x + TAMANHOBLOCO, blocos[i][j].bloco.y};
+
+            bool bateuEsquerda = CheckCollisionCircleLine(bolaPosicaoFutura, TAMANHOBOLA, esq1, esq2);
+            bool bateuDireita = CheckCollisionCircleLine(bolaPosicaoFutura, TAMANHOBOLA, dir1, dir2);
+            bool bateuCima = CheckCollisionCircleLine(bolaPosicaoFutura, TAMANHOBOLA, cima1, cima2);
+            bool bateuBaixo = CheckCollisionCircleLine(bolaPosicaoFutura, TAMANHOBOLA, baixo1, baixo2);
+
+            float raio = TAMANHOBOLA;
+
+            // LADO ESQUERDO
+            if (bateuEsquerda) {
+                bolaPosicao.x = blocos[i][j].bloco.x - raio;  
+                diagonal *= -1;
+            }
+            // LADO DIREITO
+            else if (bateuDireita) {
+                bolaPosicao.x = blocos[i][j].bloco.x + blocos[i][j].bloco.width + raio;
+                diagonal *= -1;
+            }
+            // TOPO
+            else if (bateuCima) {
+                bolaPosicao.y = blocos[i][j].bloco.y - raio;
+                direcao *= -1;
+            }
+            // BASE
+            else if (bateuBaixo) {
+                bolaPosicao.y = blocos[i][j].bloco.y + blocos[i][j].bloco.height + raio;
+                direcao *= -1;
+            }
+
+
+            if (CheckCollisionCircleRec(bolaPosicao, TAMANHOBOLA, blocos[i][j].bloco)) {
+
+                bool bateuEsquerda = CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, esq1, esq2);
+                bool bateuDireita  = CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, dir1, dir2);
+
+                // Teste topo/base
+                bool bateuTopo = bolaPosicao.y < blocos[i][j].bloco.y;
+                bool bateuBase = bolaPosicao.y > blocos[i][j].bloco.y + ALTURABLOCO;
+
+                if (bateuEsquerda || bateuDireita) {
+                    // Lateral
+                    diagonal *= -1;
+                }
+                else if (bateuTopo || bateuBase) {
+                    // Vertical
+                    direcao *= -1;
+                }
+                else {
+                    // Bateu na quina
+                    diagonal *= -1;
+                    direcao  *= -1;
+                }
+
+                // Remove o bloco
+                blocos[i][j].bloco.x = -200.0f;
+                blocos[i][j].bloco.y = -200.0f;
+            }
+        }
     }
-    
-    if (CheckCollisionCircleLine(bolaPosicao, 10.0f, plataformaPosicao, plataformaPosicaoFim)) {
-      cor = LIME;
-    }
+
 
 
     // Começa a pintar
@@ -108,11 +203,13 @@ int main() {
     ClearBackground(RAYWHITE);
     DrawText(TextFormat("%f", plataformaPosicao.x), 20, 20, 20, BLACK);
     DrawText(TextFormat("%f", bolaPosicao.y), 20, 50, 20, BLACK);
-    for (int i = 0; i < 6; i++) {
-      DrawRectangle(blocos[i].bloco.x, blocos[i].bloco.y, blocos[i].bloco.width, blocos[i].bloco.height, blocos[i].cor);
+    for (int i = 0; i < QUANTIDADELINHASBLOCOS; i++) {
+      for (int j = 0; j < QUANTIDADEBLOCOS; j++) {
+        DrawRectangle(blocos[i][j].bloco.x, blocos[i][j].bloco.y, blocos[i][j].bloco.width, blocos[i][j].bloco.height, blocos[i][j].cor);
+      }
     }
     DrawRectangleV(plataformaPosicao, plataformaTamanho, cor);
-    DrawCircleV(bolaPosicao, 10, GREEN);
+    DrawCircleV(bolaPosicao, TAMANHOBOLA, GREEN);
     
     EndDrawing();
   }
