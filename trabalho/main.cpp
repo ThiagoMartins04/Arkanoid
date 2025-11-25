a5e0174f3e83f011e3095da35948f32302ad79d6
Thiago Martins <108444333+ThiagoMartins04@users.noreply.github.com>
2025-11-24 22:09:12 -0300

    Iniciando código do menu
    Iniciando implementação do Menu para o jogo

--- trabalho/main.cpp
+++ trabalho/main.cpp
@@ -5,14 +5,19 @@
 
 using namespace std;
 
+// CONSTANTES - JOGO
+#define SCREENWIDTH 800
+#define SCREENHEIGHT 600
 // CONSTANTES - BLOCOS
 #define QUANTIDADEBLOCOS 12
 #define TAMANHOBLOCO 65.0f
 #define ALTURABLOCO 50.0f
 #define QUANTIDADELINHASBLOCOS 3
 // CONSTANTES - BOLA
-#define SPEEDBOLA 3.0f
+#define SPEEDBOLA 5.0f
 #define TAMANHOBOLA 10.0f
+// CONSTANTES - PLAYER
+#define SPEEDPLAYER 10.0f
 
 // Plataforma (player)
 Vector2 plataformaPosicao = {350.0f, 550.0f};
@@ -30,6 +35,10 @@ struct Player
 Vector2 bolaPosicao = {400.0f, 539.0f};
 Vector2 bolaPosicaoFutura;
 
+// Botões
+Rectangle botaoSair = {400.0f, 300.0f, 100.0f, 50.0f};
+const char botaoSairTexto[5] = "Sair";
+
 // Movimentação da bola
 int direcao = -1;
 int diagonal = 1; // 1 - Direita | -1 - Esquerda
@@ -43,16 +52,27 @@ struct Blocos {
   bool ativo = true;
 };
 
+// FUNCOES
+
+void movimentacaoPlayer() {
+  if ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) && plataformaPosicao.x < SCREENWIDTH - 110) {
+      plataformaPosicao.x += SPEEDPLAYER;
+      plataformaPosicaoFim.x += SPEEDPLAYER;
+    }
+    if ((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))  && plataformaPosicao.x > 10){
+      plataformaPosicao.x -= SPEEDPLAYER;
+      plataformaPosicaoFim.x -= SPEEDPLAYER;
+    }
+}
+
 int main() {
-  const int screenWidth = 800;
-  const int screenHeight = 600;
-  InitWindow(screenWidth, screenHeight, "raylib basic window");
+  InitWindow(SCREENWIDTH, SCREENHEIGHT, "raylib basic window");
   SetTargetFPS(60);
 
   //Blocos do ARKANOID
   Blocos blocos[3][12];
   Color coresBloco = GREEN;
-  float posBlocosX = (screenWidth - (QUANTIDADEBLOCOS * TAMANHOBLOCO)) / 2;
+  float posBlocosX = (SCREENWIDTH - (QUANTIDADEBLOCOS * TAMANHOBLOCO)) / 2;
   float posBlocosY = 10.0f;
   int cont = 1;
   for (int i = 0; i < QUANTIDADELINHASBLOCOS; i++) {
@@ -74,100 +94,115 @@ int main() {
     posBlocosY += 50.0f;
   }
 
+  int vidasPlayer = 3;
+  int escolha = 0;
+
   while (!WindowShouldClose()) {
-    // Movimentação do player
-    if ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) && plataformaPosicao.x < screenWidth - 110) {
-      plataformaPosicao.x += 10.0f;
-      plataformaPosicaoFim.x += 10.0f;
-    }
-    if ((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))  && plataformaPosicao.x > 10){
-      plataformaPosicao.x -= 10.0f;
-      plataformaPosicaoFim.x -= 10.0f;
-    }
+    if (vidasPlayer == 0) break;
 
-    // Movimentação da bola
-
-    //Se bater na plataforma:
-    if (CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, plataformaPosicao, plataformaPosicaoFim)) {
-      direcao *= -1;
-      // Vai verificar se a posição que a bola bateu na plataforma está mais a esqueda ou direita
-      if (bolaPosicao.x - plataformaPosicao.x < 50) {
-        //Mais a esquerda
-        bolaVelocidadeX = SPEEDBOLA * ((50 - (bolaPosicao.x - plataformaPosicao.x)) * 0.02f);
-        moveDiagonal = true;
-        diagonal = -1;
-      }
-      if (bolaPosicao.x - plataformaPosicao.x > 50) {
-        //Mais a direita
-        bolaVelocidadeX = SPEEDBOLA * ((bolaPosicao.x - plataformaPosicao.x - 50) / 50);
-        moveDiagonal = true;
-        diagonal = 1;
+    if (IsKeyPressed(MOUSE_BUTTON_LEFT)) {
+      Vector2 posicaoMouse = GetMousePosition();
+      if(CheckCollisionPointRec(posicaoMouse, botaoSair)) {
+        escolha = 1;
       }
     }
 
-    if(CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, {0.0f, 10.0f}, {800.0f, 10.0f})) {
-      direcao *= -1; //Se bater no canto superior vai pra baixo
-    }
-    if(CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, {10.0f, 0.0f}, {10.0f, 600.0f})) {
-      diagonal = 1; // Se bater no canto esquerdo vai para a direita
-    }
-    if(CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, {790.0f, 0.0f}, {790.0f, 600.0f})) {
-      diagonal = -1; // Se bater no cando direito vai para a esquerda
-    }
+    switch (escolha)
+    {
+    case 1:
+      // Movimentação do player
+      movimentacaoPlayer();
 
-    bolaPosicao.y = bolaPosicao.y + (bolaVelocidadeY * direcao);
-    if(moveDiagonal) {
-      bolaPosicao.x = bolaPosicao.x + (bolaVelocidadeX * diagonal);
-    }
-
-    // Pega a posição futura para evitar que a bola ultrapasse o bloco
-    if (moveDiagonal) {
-      bolaPosicaoFutura = {bolaPosicao.x + bolaVelocidadeX, bolaPosicao.y + SPEEDBOLA};
-    } else {
-      bolaPosicaoFutura = {bolaPosicao.x, bolaPosicao.y + SPEEDBOLA};
-    }
-
-    // Verifica colisão
-    Vector2 futuraX = { bolaPosicao.x + (bolaVelocidadeX * diagonal), bolaPosicao.y };
-    Vector2 futuraY = { bolaPosicao.x, bolaPosicao.y + (bolaVelocidadeY * direcao) };
-
-    bool houveColisao = false;
-
-    for (int i = 0; i < QUANTIDADELINHASBLOCOS && !houveColisao; i++) {
-        for (int j = 0; j < QUANTIDADEBLOCOS && !houveColisao; j++) {
-
-            Rectangle r = blocos[i][j].bloco;
-            if (r.x < 0) continue;
-
-            bool colisaoX = CheckCollisionCircleRec(futuraX, TAMANHOBOLA, r);
-            bool colisaoY = CheckCollisionCircleRec(futuraY, TAMANHOBOLA, r);
-
-            if (colisaoX || colisaoY) {
-
-                if (colisaoX && !colisaoY) {
-                    // bateu na lateral
-                    diagonal = -diagonal;
-                }
-                else if (colisaoY && !colisaoX) {
-                    // bateu por cima ou por baixo
-                    direcao = -direcao;
-                }
-                else {
-                    // bateu exatamente na quina → escolha a NORMAL do bloco
-                    if (fabs((bolaPosicao.x - (r.x + r.width/2))) >
-                        fabs((bolaPosicao.y - (r.y + r.height/2))))
-                        diagonal = -diagonal;
-                    else
-                        direcao = -direcao;
-                }
-
-                blocos[i][j].bloco.x = -200;
-                blocos[i][j].bloco.y = -200;
-
-                houveColisao = true;
-            }
+      // Movimentação da bola
+      //Se bater na plataforma:
+      if (CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, plataformaPosicao, plataformaPosicaoFim)) {
+        direcao *= -1;
+        // Vai verificar se a posição que a bola bateu na plataforma está mais a esqueda ou direita
+        if (bolaPosicao.x - plataformaPosicao.x < 50) {
+          //Mais a esquerda
+          bolaVelocidadeX = SPEEDBOLA * ((50 - (bolaPosicao.x - plataformaPosicao.x)) * 0.02f);
+          moveDiagonal = true;
+          diagonal = -1;
         }
+        if (bolaPosicao.x - plataformaPosicao.x > 50) {
+          //Mais a direita
+          bolaVelocidadeX = SPEEDBOLA * ((bolaPosicao.x - plataformaPosicao.x - 50) / 50);
+          moveDiagonal = true;
+          diagonal = 1;
+        }
+      }
+
+      if(CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, {0.0f, 10.0f}, {800.0f, 10.0f})) {
+        direcao *= -1; //Se bater no canto superior vai pra baixo
+      }
+      if(CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, {10.0f, 0.0f}, {10.0f, 600.0f})) {
+        diagonal = 1; // Se bater no canto esquerdo vai para a direita
+      }
+      if(CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, {790.0f, 0.0f}, {790.0f, 600.0f})) {
+        diagonal = -1; // Se bater no canto direito vai para a esquerda
+      }
+      if(CheckCollisionCircleLine(bolaPosicao, TAMANHOBOLA, {0.0f, (600.0f + TAMANHOBOLA * 2)}, {790.0f, (600.0f + TAMANHOBOLA * 2)})) {
+        direcao *= -1; // Se sair do jogo perde uma vida
+        vidasPlayer--;
+      }
+
+      bolaPosicao.y = bolaPosicao.y + (bolaVelocidadeY * direcao);
+      if(moveDiagonal) {
+        bolaPosicao.x = bolaPosicao.x + (bolaVelocidadeX * diagonal);
+      }
+
+      // Pega a posição futura para evitar que a bola ultrapasse o bloco
+      if (moveDiagonal) {
+        bolaPosicaoFutura = {bolaPosicao.x + bolaVelocidadeX, bolaPosicao.y + SPEEDBOLA};
+      } else {
+        bolaPosicaoFutura = {bolaPosicao.x, bolaPosicao.y + SPEEDBOLA};
+      }
+
+      // Verifica colisão
+      Vector2 futuraX = { bolaPosicao.x + (bolaVelocidadeX * diagonal), bolaPosicao.y };
+      Vector2 futuraY = { bolaPosicao.x, bolaPosicao.y + (bolaVelocidadeY * direcao) };
+
+      bool houveColisao = false;
+
+      for (int i = 0; i < QUANTIDADELINHASBLOCOS && !houveColisao; i++) {
+          for (int j = 0; j < QUANTIDADEBLOCOS && !houveColisao; j++) {
+
+              Rectangle r = blocos[i][j].bloco;
+              if (r.x < 0) continue;
+
+              bool colisaoX = CheckCollisionCircleRec(futuraX, TAMANHOBOLA, r);
+              bool colisaoY = CheckCollisionCircleRec(futuraY, TAMANHOBOLA, r);
+
+              if (colisaoX || colisaoY) {
+
+                  if (colisaoX && !colisaoY) {
+                      // bateu na lateral
+                      diagonal = -diagonal;
+                  }
+                  else if (colisaoY && !colisaoX) {
+                      // bateu por cima ou por baixo
+                      direcao = -direcao;
+                  }
+                  else {
+                      // bateu exatamente na quina → escolha a NORMAL do bloco
+                      if (fabs((bolaPosicao.x - (r.x + r.width/2))) >
+                          fabs((bolaPosicao.y - (r.y + r.height/2))))
+                          diagonal = -diagonal;
+                      else
+                          direcao = -direcao;
+                  }
+
+                  blocos[i][j].bloco.x = -200;
+                  blocos[i][j].bloco.y = -200;
+
+                  houveColisao = true;
+              }
+          }
+      }
+      break;
     }
+
+    
 
 
 
@@ -178,8 +213,11 @@ int main() {
     // Começa a pintar
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText(TextFormat("%f", plataformaPosicao.x), 20, 20, 20, BLACK);
-    DrawText(TextFormat("%f", bolaPosicao.y), 20, 50, 20, BLACK);
+
+    DrawRectangleRec(botaoSair, GRAY);
+    DrawText(botaoSairTexto, botaoSair.x, botaoSair.y, 20, BLACK);
+    
+    
     for (int i = 0; i < QUANTIDADELINHASBLOCOS; i++) {
       for (int j = 0; j < QUANTIDADEBLOCOS; j++) {
         DrawRectangle(blocos[i][j].bloco.x, blocos[i][j].bloco.y, blocos[i][j].bloco.width, blocos[i][j].bloco.height, blocos[i][j].cor);
@@ -187,6 +225,10 @@ int main() {
     }
     DrawRectangleV(plataformaPosicao, plataformaTamanho, cor);
     DrawCircleV(bolaPosicao, TAMANHOBOLA, GREEN);
+
+    DrawText(TextFormat("%f", plataformaPosicao.x), 0, 500, 20, BLACK);
+    DrawText(TextFormat("%f", bolaPosicao.y), 700, 550, 20, BLACK);
+    DrawText(TextFormat("%i", vidasPlayer), 700, 500, 20, BLACK);
     
     EndDrawing();
   }
